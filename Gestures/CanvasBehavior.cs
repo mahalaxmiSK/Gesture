e290533672e3d11186124a62ec0b06a6a405f003
mahalaxmiSK <mahalaxmi.kumbari@gmail.com>
2022-01-03 23:03:17 +0530

    Add files via upload

---
+++ Gestures/CanvasBehavior.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gestures
+{
+    public class CanvasBehavior : Behavior<Canvas>
+    {
+        protected Point SwipeStart;
+        protected List<TouchPoints> touchPointCllection = new List<TouchPoints>();
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.TouchUp += AssociatedObject_TouchUp;
+            AssociatedObject.TouchDown += AssociatedObject_TouchDown;
+            AssociatedObject.ManipulationCompleted += AssociatedObject_ManipulationCompleted;
+        }
+
+        private void AssociatedObject_ManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
+        {
+            if (touchPointCllection.Any() && touchPointCllection.All(_ => _.swipeType == touchPointCllection.First().swipeType))
+            {
+                swipeGesture gesture = touchPointCllection.First().swipeType;
+                if (touchPointCllection.Count == 1 && gesture != swipeGesture.Down)
+                {
+                    if (gesture == swipeGesture.Left)
+                    {
+                        File.AppendAllText(@"D:\Trace.txt", $"Swiped Left" + Environment.NewLine);
+                    }
+                    else
+                    {
+                        File.AppendAllText(@"D:\Trace.txt", $"Swiped Right" + Environment.NewLine);
+                    }
+                }
+                else if (touchPointCllection.Count == 2 && gesture == swipeGesture.Down)
+                {
+                    File.AppendAllText(@"D:\Trace.txt", $"2 finger Swiped Down" + Environment.NewLine);
+                }
+            }
+            touchPointCllection.Clear();
+        }
+
+        private void AssociatedObject_TouchDown(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            SwipeStart = e.GetTouchPoint(AssociatedObject).Position;
+        }
+
+        private void AssociatedObject_TouchUp(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            var Swipe = e.GetTouchPoint(AssociatedObject).Position;
+
+            //Swipe Right
+            if (Swipe.X > (SwipeStart.X + 200))
+            {
+                touchPointCllection.Add(new TouchPoints { DownPoint = SwipeStart, UpPoint = Swipe, swipeType = swipeGesture.Right });
+            }
+            //Swipe Left
+            else if (Swipe.X < (SwipeStart.X - 200))
+            {
+                touchPointCllection.Add(new TouchPoints { DownPoint = SwipeStart, UpPoint = Swipe, swipeType = swipeGesture.Left });
+            }
+            //Swipe Down
+            else if (Swipe.Y > (SwipeStart.Y + 200))
+            {
+                touchPointCllection.Add(new TouchPoints { DownPoint = SwipeStart, UpPoint = Swipe, swipeType = swipeGesture.Down });
+            }
+            e.Handled = true;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.TouchUp -= AssociatedObject_TouchUp;
+            AssociatedObject.TouchDown -= AssociatedObject_TouchDown;
+            AssociatedObject.ManipulationCompleted -= AssociatedObject_ManipulationCompleted;
+        }
+
+    }
+    public class TouchPoints
+    {
+        public Point UpPoint { get; set; }
+        public Point DownPoint { get; set; }
+        public swipeGesture swipeType { get; set; }
+    }
+    public enum swipeGesture
+    {
+        Right,
+        Left,
+        Down
+    }
+}
---
+++ Gestures/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.IO;
+using System.Windows.Media.Animation;
+using Microsoft.Ink;
+using System;
+
+namespace Gestures
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+            InkCanvas.DefaultDrawingAttributes.Width = 15;
+            InkCanvas.DefaultDrawingAttributes.StylusTip = System.Windows.Ink.StylusTip.Ellipse;
+            InkCanvas.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("White");
+            btnshow.Foreground = Brushes.Black;
+            btnshow.Content = "\u21C9";
+            btnshow.Click += Btnshow_Click;
+        }
+        private bool isShow;
+
+        private void Btnshow_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isShow)
+            {
+                Storyboard sb = Resources["OpenMenu"] as Storyboard;
+                sb.Begin(LeftMenu);
+            }
+            else
+            {
+                Storyboard sb = Resources["CloseMenu"] as Storyboard;
+                sb.Begin(LeftMenu);
+            }
+            isShow = !isShow;
+        }
+
+        private void InkCanvas_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            HandleGesture();
+        }
+        private void InkCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            HandleGesture();
+        }
+        private void HandleGesture()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                InkCanvas.Strokes.Save(ms);
+                var ink = new Ink();
+                ink.Load(ms.ToArray());
+
+                using (RecognizerContext context = new RecognizerContext())
+                {
+                    if (ink.Strokes.Count > 0)
+                    {
+                        context.Strokes = ink.Strokes;
+                        RecognitionStatus status;
+
+                        var result = context.Recognize(out status);
+
+                        if (status == RecognitionStatus.NoError)
+                        {
+                            string letter = result.TopString;
+                            File.AppendAllText(@"D:\Trace.txt", $"Letter wriiten is {letter}" + Environment.NewLine);
+
+                        }
+                    }
+                }
+            }
+            InkCanvas.Strokes.Clear();
+        }
+    }
+}
